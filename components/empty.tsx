a07368818c9ed5ed3f1edea1c2e9a802f6fedf1b
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2024-03-19 18:01:47 +0500

    saas-ai

---
+++ components/empty.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+interface EmptyProps {
+    label: string;
+}
+
+const Empty = ({
+    label
+}: EmptyProps) => {
+  return (
+    <div className="h-full p-20 flex flex-col items-center justify-center">
+        <div className="relative h-72 w-72">
+            <Image
+            src="/empty.png"
+            alt="Empty"
+            layout="fill"
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Empty
---
+++ components/user-avatar.tsx
@@ -0,0 +1,22 @@
+import { useUser } from "@clerk/nextjs"
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+
+
+const UserAvatar = () => {
+  const { user } = useUser();
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src={user?.profileImageUrl} />
+        <AvatarFallback>
+          {user?.firstName?.charAt(0)}
+          {user?.lastName?.charAt(0)}
+        </AvatarFallback>
+      </Avatar>
+    </div>
+  )
+}
+
+
+export default UserAvatar
